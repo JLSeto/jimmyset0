8e36152b67692a0726bbb361a0a256c667b78846
jlseto <setojs0@gmail.com>
2022-02-23 23:15:05 -0800

    updates

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -38,7 +38,7 @@ export class ProjectsComponent implements OnInit
                 details        :    [
                                         { 
                                             heading   : 'Overview', 
-                                            notes     : `I built a rhythm game over the weekend for fun.
+                                            notes     : `I built this rhythm game over the weekend for fun.
                                                         This rhythm game is similar to other rhythm games where you have to press buttons/keys
                                                         in a sequence that is shown on the screen with some music.  
                                                         While I used the League of Legends art & music for this demo, 
