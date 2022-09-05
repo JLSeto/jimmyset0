3ec80ec6da67a5604239255a94c2da546e4d84b5
jlseto <setojs0@gmail.com>
2022-09-04 17:12:41 -0700

    asdf

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -2,8 +2,8 @@
 # ng build --configuration production --output-path docs --base-href https://jlseto.github.io
 sudo ng build --configuration production --output-path docs --base-href https://jimmyseto.com
 sudo cp docs/index.html docs/404.html
-touch docs/.nojekyll
-touch docs/CNAME
+sudo touch docs/.nojekyll
+sudo touch docs/CNAME
 echo "jimmyseto.com" >> docs/CNAME
 git add docs/*
 git commit -am "updates"
